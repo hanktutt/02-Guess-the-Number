71255cd43ff6680adc15cef893f7114aac4cbd2b
hanktutt <35299445+hanktutt@users.noreply.github.com>
2018-01-30 10:56:24 -0500

    I am finishing mywork

--- guess.py
+++ guess.py
@@ -7,23 +7,37 @@ assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 #variables
 guesses = 10
 guess_range = 20
-
+quit = False
 
 #generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
-number = random.randint(1,guess_range)
+while quit == False:
+	number = random.randint(1,guess_range)
 
-#ask the user for a response and store it in the variable [guess]
-guess = input()
+	#ask the user for a response and store it in the variable [guess]
 
 
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
 
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
+	#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
+	for count in range(guesses):
+		try:
+			guess = input('Please enter a number 1-{0}: '.format(guess_range))
+		#convert the guess to an integer
+			guess = int(guess)
+				#check if the guess is less than the random number
+			if guess < number:
+				print('Too low!')
+				#check if the guess is more than the random number
+			if guess > number:
+				print('Too high!')
+			if guess == number:
+				print('You Win!')
+				break
+		except ValueError:
+			print('Please enter a whole number.')
+	play_again = input('Do You want to play again Y/N?')
+	if play_again.upper() == 'Y':
+		print('Ready?')
+	if play_again.upper() == 'N':
+		quit = True
 
-except ValueError:
-	print('Please enter a whole number.')
+print('see you later!')
